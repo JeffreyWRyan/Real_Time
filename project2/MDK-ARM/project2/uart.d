6fb44752965ce0a595e7d77999f1de1754265a44
JeffreyWRyan <46799281+JeffreyWRyan@users.noreply.github.com>
2019-02-25 11:25:19 -0500

    Initial commit of Project2.  USART not working

---
+++ project2/MDK-ARM/project2/uart.d
@@ -0,0 +1,10 @@
+project2\uart.o: ..\Src\UART.c
+project2\uart.o: ..\Src\UART.h
+project2\uart.o: ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h
+project2\uart.o: ../Drivers/CMSIS/Include/core_cm4.h
+project2\uart.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+project2\uart.o: ../Drivers/CMSIS/Include/core_cmInstr.h
+project2\uart.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+project2\uart.o: ../Drivers/CMSIS/Include/core_cmFunc.h
+project2\uart.o: ../Drivers/CMSIS/Include/core_cmSimd.h
+project2\uart.o: ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h
